42bfc98e7c14cc3f99a9ba2ead2d66fbcc441ae5
ArthurWinglersebbmen <arthur.w06@aluno.ifsc.edu.br>
2026-03-23 16:42:03 -0300

    ygwr

---
+++ sitesuperchock/js/menu.js
@@ -0,0 +1,14 @@
+function showMenu() {
+    alert("gewkjoqegijhqgi")
+    const menulateral = document.getElementById("menu-lateral")
+    const iconmenu = document.getElementById("img-menu")
+
+    menulateral.classList.toggle('ativa');
+
+    if(menulateral.classList.contains('ativa')){
+        iconmenu.src = "img/icon-close-menu.png"
+    } else {
+        iconmenu.src = "img/icon-hamburger-menu.png"
+    }
+}
+    
