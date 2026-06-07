e00f881f160577795df52a2d96d86fc8db9d9f43
ArthurWinglersebbmen <arthur.w06@aluno.ifsc.edu.br>
2026-06-07 18:51:09 -0300

    Update db_pweb1_frota.sql

---
+++ trabalho_2/database/db_pweb1_frota.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE IF NOT EXISTS db_pweb1_frota;
+USE db_pweb1_frota;
+
+-- Tabela Padrão do Sistema (Obrigatória)
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    login VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+-- Inserindo o usuário padrão para testes exigido pelo professor
+INSERT INTO usuario (nome, telefone, email, login, senha) 
+VALUES ('Administrador', '(00) 00000-0000', 'admin@sistema.com', 'admin', '123');
+
+-- Tabela do CRUD 1: Veículos
+CREATE TABLE IF NOT EXISTS veiculo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    placa VARCHAR(10) NOT NULL UNIQUE,
+    modelo VARCHAR(100) NOT NULL,
+    ano_fabricacao INT NOT NULL,
+    capacidade_passageiros INT NOT NULL
+);
+
+-- Tabela do CRUD 2: Motoristas
+CREATE TABLE IF NOT EXISTS motorista (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_motorista VARCHAR(100) NOT NULL,
+    numero_cnh VARCHAR(20) NOT NULL UNIQUE,
+    categoria_cnh VARCHAR(5) NOT NULL,
+    validade_exame_medico DATE NOT NULL
+);
+
+-- Tabela do CRUD 3: Manutenções
+CREATE TABLE IF NOT EXISTS manutencao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_servico DATE NOT NULL,
+    quilometragem_atual INT NOT NULL,
+    descricao_pecas TEXT NOT NULL,
+    custo_total DECIMAL(10, 2) NOT NULL
+);
